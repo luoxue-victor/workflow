8c52a412628648fa92e0cb5ea56a48fe0b65c938
罗学 <luoxue.luoxue@autonavi.com>
2021-04-06 15:05:49 +0800

    update

--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-// jest.setup.js
-/* eslint-disable import/no-extraneous-dependencies */
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
