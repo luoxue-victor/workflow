049b0330af3c4cc497de012317d7b70a5afc8c10
罗学 <luoxue.luoxue@autonavi.com>
2021-04-12 17:13:21 +0800

    update

--- docs/readme/index.js
+++ docs/readme/index.js
@@ -9,7 +9,6 @@ const exec = require('child_process').execSync;
 // 执行 packages 内生成readme
 packagePaths.forEach(_ => {
   const pkgReadmePath = path.join(packagesDirPath, _, 'readme', 'index.js')
-
   if (fs.existsSync(pkgReadmePath)) {
     exec(`node ${pkgReadmePath}`)
   }
