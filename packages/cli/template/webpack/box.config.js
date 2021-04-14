da9f968b420a572b9bb7d5409d73dc13a91958d0
罗学 <luoxue.luoxue@autonavi.com>
2021-04-14 16:01:01 +0800

    fix: 修复 webpack tempalte

---
+++ packages/cli/template/webpack/box.config.js
@@ -0,0 +1,96 @@
+const path = require('path')
+function resolve (dir) {
+  return path.join(process.cwd(), dir)
+}
+
+module.exports = function (config) {
+  /**
+   * @param {object} dll 开启差分包
+   * @param {object} pages 多页面配置 通过 box run/build index 来使用
+   * @param {function} chainWebpack
+   * @param {string} entry 入口
+   * @param {string} output 出口
+   * @param {string} publicPath
+   * @param {string} port 端口
+   * @param {object} eslint eslint 配置
+   * @param {object} stylelint stylelint 配置
+   * @param {object} eslint eslint 配置
+   * @param {object} alias 配置别名
+   * @param {object} env 配置通用变量，可以在 node 跟 web 之间共同使用
+   * @param {Boolean} filenameHashing 文件名是否使用 hash，当文件发生变动的时候 filename 才会改变
+   * @param {Boolean} css 配置 css
+   * @param {Boolean} mock 开启 mock
+   */
+  return {
+    pages: {
+      index1: {
+        entry: 'src/main.js',
+        template: 'public/index.html',
+        filename: 'index.html',
+        publicPath: './',
+        output: 'dist/index1'
+      },
+      index2: {
+        entry: 'src/main.js',
+        template: 'public/index2.html',
+        filename: 'index2.html',
+        publicPath: './',
+        output: 'dist/index2'
+      }
+    },
+    port: 9001,
+    mock: true,
+    env: {
+      REACT: 'react' // 配置 react
+    },
+    alias: {
+      '@': resolve('src'),
+      '@src': resolve('src')
+    },
+    resources: {
+      less: {
+        patterns: [
+          path.resolve(__dirname, './src/global/*.less')
+        ]
+      },
+      scss: {
+        patterns: [
+          path.resolve(__dirname, './src/global/*.scss')
+        ]
+      }
+    },
+    css: {
+      sourceMap: true,
+      loaderOptions: {
+        css: {},
+        less: {
+          globalVars: {
+            gray: '#ccc'
+          }
+        },
+        sass: {},
+        postcss: {},
+        stylus: {}
+      },
+      isCssModule: false, // 是否对css进行模块化处理
+      needInlineMinification: false // 是否需要压缩css
+    },
+    filenameHashing: true,
+    eslint: {
+      lintOnSave: true, // 开启运行时检测
+      extensions: ['js', 'jsx', 'vue'] // 默认 ['js', 'jsx']
+    },
+    tslint: {
+      lintOnSave: true, // 开启运行时检测
+      useThreads: true
+    },
+    stylelint: {
+      lintOnSave: true // 开启运行时检测
+      // extensions: ['vue', 'htm', 'html', 'css', 'sss', 'less', 'scss']
+    },
+    // dll: {
+    //   venders: ['react']
+    // },
+    chainWebpack(config) {}
+  }
+}
